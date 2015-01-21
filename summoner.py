ca6cd4fa8466fdc57cf022eba188772eb89e2f52
Jeremy Neptune <jerenept@gmail.com>
2015-01-21 19:28:09 -0400

    added a class and methods to retreive summoner data. i am disturbed by the repetition involved

--- summoner.py
+++ summoner.py
@@ -25,7 +25,7 @@
 
 import requests
 
-Class Summoner(object):
+class Summoner(object):
 	"""Objects representing Summoners"""
 	def __init__(self, summonerID, summonerName, profileIconID, revisionDate, summonerLevel, region):
 		self.summonerID = summonerID
@@ -35,8 +35,31 @@ Class Summoner(object):
 		self.revisionDate = revisionDate
 		self.region = region
 		
-	 def __eq__(self, other):
-		 """returns whether the other instance of summoner is the same as this one"""
-		 return (isinstance(other, Summoner)) AND (self.summonerID == other.summonerID) AND (self.region == other.region)
+	def __eq__(self, other):
+		"""returns whether the other instance of summoner is the same as this one"""
+		return (isinstance(other, Summoner)) and (self.summonerID == other.summonerID) and (self.region == other.region)
 
+class GetSummoners
 
+	def byName(summonerNames, region, api_key):
+		"""returns a list of Summoner objects, from a list of summoner names, and region"""
+		r = requests.get('http://{0}.api.pvp.net/api/lol/{0}/v1.4/summoner/by-name/{1}?api_key={2}'.format(region,summonerNames, api_key))
+		listOfSummoners = []
+		for summoner in r.json():
+			x = r.json()[summoner]
+			newSummoner = Summoner(x['id'], x['name'], x['summonerLevel'], x['profileIconId'], x['revisionDate'], x['summonerLevel'], region)
+			listOfSummoners.append(newSummoner)
+		return listOfSummoners
+	
+	def byId(summonerIds, region, api_key):
+		"""returns a list of Summoner objects, from a list of summoner ids, and region"""
+		r = requests.get('http://{0}.api.pvp.net/api/lol/{0}/v1.4/summoner/{1}?api_key={2}'.format(region,summonerNames, api_key))
+		listOfSummoners = []
+		for summoner in r.json():
+			x = r.json()[summoner]
+			newSummoner = Summoner(x['id'], x['name'], x['summonerLevel'], x['profileIconId'], x['revisionDate'], x['summonerLevel'], region)
+			listOfSummoners.append(newSummoner)
+		return listOfSummoners
+	#the fact that these functions are nearly identical is highly disturbing, and I think I should do something about it
+	
+		
