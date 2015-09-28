d0db9f068ab24c6b3d50d738b932bcfd6b043c4f
Jeremy Neptune <jerenept@gmail.com>
2015-09-27 23:42:57 -0700

    Made the Mastery object actually work

--- statics.py
+++ statics.py
@@ -22,14 +22,22 @@
 #  
 #  
 
-"""Static data for Riot Api, I should have this in a separate file for now till I decide if they would be better as parts of others or not"""
+"""
+Static data for Riot Api, I should have this in a separate file for now 
+till I decide if they would be better as parts of others or not
+"""
 
 import requests
-import api_key.API_KEY as API_KEY
-CURRENT_PATCH = requests.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/versions?API_KEY={0}".format(API_KEY)).json()[0] 
+
+import api_key
+
+API_KEY = api_key.API_KEY
+
+r = requests.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/versions?api_key={}".format(API_KEY)).json()
+
+CURRENT_PATCH = r[0]
 #the first member in the list of patches, sorted by recency, is the most current patch
-masteryRequest = requests.get("http://ddragon.leagueoflegends.com/cdn/{CURRENT_PATCH}/data/en_US/mastery.json".format(CURRENT_PATCH)) #todo: i18n
-allMasteryData = masteryRequest.json()['data']
+
 
 class Mastery:
 	"""
@@ -40,33 +48,39 @@ class Mastery:
 	
 	self.prereq contains None if there is no prerequisite mastery.
 	""" 
-	
+	allMasteryData = requests.get("http://ddragon.leagueoflegends.com/cdn/{}/data/en_US/mastery.json".format(CURRENT_PATCH)).json()['data'] #todo:i18n
 	def __init__(self, masteryId, patch = CURRENT_PATCH):
 		"""Initializes the Mastery object with data about that particular mastery."""
-		self.masteryId = str(masteryId)
-		MasteryData = allMasteryData[self.masteryId]
-		self.name = masteryData['name']
-		self.description = masteryData['description'] #this is a list of the mastery's possibly numerous descriptions'		
-		self.imageURL = "http://ddragon.leagueoflegends.com/cdn/{0}/img/mastery/{1}.png".format(patch, self.masteryID)
-		self.ranks = masteryData['ranks'] 	#this should be the length of the list in self.description
 		
-		if (masteryData['prereq']=='0'):
+		self.masteryId = str(masteryId)
+		
+		#this block sets the member variables according to the JSON info we got 
+		#from the DataDragon API
+		
+		self.masteryData = self.allMasteryData[self.masteryId]
+		self.name = self.masteryData['name'] #name of mastery
+		self.description = self.masteryData['description'] 
+		
+		#this is a list of the mastery's possibly numerous descriptions, as the 
+		#descriptions may change depending on rank.
+				
+		self.imageURL = "http://ddragon.leagueoflegends.com/cdn/{0}/img/mastery/{1}.png".format(patch, self.masteryId)
+		
+		self.ranks = self.masteryData['ranks'] 	#this should be the length of the list in self.description
+		
+		if (self.masteryData['prereq']=='0'):
 			self.prereq = None
 		else:
-			self.prereq = Mastery(masteryData['prereq'], patch)
+			self.prereq = self.Mastery(masteryData['prereq'], patch)
 		#self.prereq is the mastery that must be active for
 		#this one to be used.
-	def __unicode__(self):
-		"""Returns a Unicode String describing the Mastery Object."""
+	def __str__(self):
+		"""Returns a (Unicode) String describing the Mastery Object."""
 		return self.name
-
+	
 	def __eq__(self, other):
-		"""Returns whether two mastery objects actually describe the same Mastery"""
-		assert isinstance(other, Mastery), "Invalid Comparison with\
-		Mastery object."
+		"""Returns whether two mastery objects (specifically) actually describe the same Mastery"""
+		assert isinstance(other, Mastery), "Invalid Comparison with	Mastery object."
+		#there really isn't anything else a mastery can conceivably be compared with
+		#and False may be a misleading return.
 		return (self.masteryId == other.masteryId)
-	
-
-		
-	
-	
